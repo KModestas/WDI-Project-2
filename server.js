da22f344a4900ad5cf766ffb8b411767168eb512
Modestas <modestask97@gmail.com>
2018-01-16 20:06:36 +0000

    required some important packages in server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express        = require('express');
 const morgan         = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
-const routes         = require('./config/routes');
-const mongoose       = require('mongoose');
 const bodyParser     = require('body-parser');
-mongoose.Promise     = require('bluebird');
-const { port, env, dbURI, secret } = require('./config/environment');
-const session = require('express-session');
-const flash = require('express-flash');
-
+const mongoose       = require('mongoose');
+const methodOverride = require('method-override');
+const session        = require('express-session');
+const flash          = require('express-flash');
+const routes         = require('./config/routes');
+const customResponses = require('./lib/customResponses');
+const authentication = require('./lib/authentication');
+const errorHandler = require('./lib/errorHandler');
 
 const app = express();
+const { port, env, dbURI, secret } = require('./config/environment');
 
+mongoose.Promise  = require('bluebird');
 mongoose.connect(dbURI);
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
-
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 if(env === 'development') app.use(morgan('dev'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
   secret: secret,
@@ -31,6 +31,19 @@ app.use(session({
 
 app.use(flash());
 
+app.use(customResponses);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride(function (req) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use(authentication);
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
